45f41d951b896db6ede27ed0a55e61f9b66e63d9
Michael Sumner <mdsumner@gmail.com>
2016-07-25 20:50:18 +1000

    very basic raw boilerplate, and minimal SQL example

--- R/GDALSQL.R
+++ R/GDALSQL.R
@@ -33,26 +33,68 @@ setClass("GDALSQLConnection",
          slots = list(
            host = "character",
            username = "character",
-           # and so on
-           ptr = "externalptr"
+           dbname = "character",
+           rgdal2DS = "RGDAL2Datasource"
          )
 )
 
 
+#' dbConnect
+#'
+#' dbConnect
+#'
 #' @param drv An object created by \code{GDALSQL()}
 #' @export
 #' @importFrom rgdal2 openOGR
-#' @rdname GDALSQL
+#' @rdname GDALSQL-dbConnect
 #' @examples
 #' \dontrun{
-#' db <- dbConnect(RGDALSQL::GDALSQL())
+#' db <- dbConnect(RGDALSQL::GDALSQL(), afile)
 #' afile <- system.file("extdata", "shapes.gpkg", package = "RGDALSQL")
+#' con <- rgdal2::openOGR(afile)
 #' #dbWriteTable(db, "mtcars", mtcars)
 #' #dbGetQuery(db, "SELECT * FROM mtcars WHERE cyl == 4")
 #' }
 setMethod("dbConnect", "GDALSQLDriver",
-          function(drv, dsn = "", ...) {
+          function(drv, dbname = "", readonly = TRUE, ...) {
   # ...
-
-  new("GDALSQLConnection", host = host, ...)
+  con <- rgdal2::openOGR(dbname)
+  new("GDALSQLConnection", host = "", rgdal2DS = con,  ...)
 })
+
+#' @rdname GDALSQLConnection-class
+#' @export
+setMethod("show", "GDALSQLConnection", function(object) {
+  cat("<GDALSQLConnection>\n")
+ # if (dbIsValid(object)) {
+    cat("  Path: ", object@dbname, "\n", sep = "")
+    print(object@rgdal2DS)
+  #} else {
+  #  cat("  DISCONNECTED\n")
+  #}
+})
+
+## dbDisconnect
+
+# #' GDALSQL results class
+# #'
+# #' @keywords internal
+# #' @export
+# setClass("GDALSQLResult",
+#  contains = "DBIResult",
+#  slots = list(ptr = "externalptr")
+#  )
+
+#' Send a query to GDALSQL.
+#'
+#' @export
+#' @examples
+#' f = system.file("example-data/continents", package = "rgdal2")
+#' db <- dbConnect(RGDALSQL::GDALSQL(), f)
+#' dbSendQuery(db, "SELECT * FROM continent WHERE FID < 1")
+setMethod("dbSendQuery", "GDALSQLConnection",
+          function(conn, statement, ...) {
+            layer <- rgdal2::getSQLLayer(conn@rgdal2DS, statement)
+            print(layer)
+            new("GDALSQLResult", ...)
+          })
