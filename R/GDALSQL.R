660f67ea47b4d4022c671f5af764e3a1f150cd2d
Michael Sumner <mdsumner@gmail.com>
2016-07-25 22:17:47 +1000

    package scaffolding

--- R/GDALSQL.R
+++ R/GDALSQL.R
@@ -51,6 +51,7 @@ setClass("GDALSQLConnection",
 #' \dontrun{
 #' db <- dbConnect(RGDALSQL::GDALSQL(), afile)
 #' afile <- system.file("extdata", "shapes.gpkg", package = "RGDALSQL")
+#' chuck <- rgdal::readOGR(afile, "sids") ## bizarrely this resets the problem
 #' con <- rgdal2::openOGR(afile)
 #' #dbWriteTable(db, "mtcars", mtcars)
 #' #dbGetQuery(db, "SELECT * FROM mtcars WHERE cyl == 4")
@@ -76,25 +77,28 @@ setMethod("show", "GDALSQLConnection", function(object) {
 
 ## dbDisconnect
 
-# #' GDALSQL results class
-# #'
-# #' @keywords internal
-# #' @export
-# setClass("GDALSQLResult",
-#  contains = "DBIResult",
-#  slots = list(ptr = "externalptr")
-#  )
+ #' GDALSQL results class
+ #'
+ #' @keywords internal
+ #' @export
+ setClass("GDALSQLResult",
+  contains = "DBIResult",
+  ## dummy for now to avoid null pointer error
+  slots = list(a = "character")
+  )
 
 #' Send a query to GDALSQL.
 #'
 #' @export
 #' @examples
 #' f = system.file("example-data/continents", package = "rgdal2")
+#' chuck <- rgdal::ogrInfo(f, "continent") ## bizarrely this resets the problem
 #' db <- dbConnect(RGDALSQL::GDALSQL(), f)
 #' dbSendQuery(db, "SELECT * FROM continent WHERE FID < 1")
 setMethod("dbSendQuery", "GDALSQLConnection",
           function(conn, statement, ...) {
             layer <- rgdal2::getSQLLayer(conn@rgdal2DS, statement)
             print(layer)
+
             new("GDALSQLResult", ...)
           })
